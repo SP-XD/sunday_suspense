0049725420dc5303167753e73b534348f0cfb8b5
sp-xd <69642436+SP-XD@users.noreply.github.com>
2024-07-10 18:07:20 +0530

    feat: improved splash screen and add device preview for dev flavour

--- lib/features/splash/view/splash_screen.dart
+++ lib/features/splash/view/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:midnight_suspense/gen/assets.gen.dart';
@@ -17,10 +19,10 @@ class SplashScreen extends StatelessWidget {
         child: Stack(
           children: [
             Positioned(
-              top: mediaQuery.height * 0.15,
+              top: 120,
               child: Assets.images.hauntedHouseFrame1
                   .image(
-                    fit: BoxFit.fill,
+                    fit: BoxFit.contain,
                     width: mediaQuery.width,
                   )
                   .animate(
@@ -33,11 +35,18 @@ class SplashScreen extends StatelessWidget {
                     delay: 0.ms,
                     builder: (context) {
                       return Assets.images.hauntedHouseFrame2.image(
-                        fit: BoxFit.fill,
+                        fit: BoxFit.contain,
                         width: mediaQuery.width,
                       );
                     },
                   ),
+            ),
+            Positioned(
+              top: -100,
+              left: -150,
+              child: Assets.images.cloudInverted.image(
+                height: 300,
+              ),
             ),
             Positioned(
               left: -150,
@@ -50,7 +59,7 @@ class SplashScreen extends StatelessWidget {
                       // onPlay: (controller) => controller.repeat(),
                       )
                   .slideX(
-                    duration: 1000.ms,
+                    duration: 2000.ms,
                     begin: -0.1,
                     end: 0,
                     curve: Curves.easeInOut,
@@ -58,7 +67,7 @@ class SplashScreen extends StatelessWidget {
             ),
             Positioned(
               left: 10,
-              top: mediaQuery.height * 0.05,
+              top: 40,
               child: Assets.images.moon.image(
                 height: 250,
               ),
@@ -74,18 +83,28 @@ class SplashScreen extends StatelessWidget {
                     )
                     .animate()
                     .slideX(
-                      duration: 1000.ms,
+                      duration: 2000.ms,
                       begin: 0,
                       end: -0.1,
-                      curve: Curves.easeInOut,
+                      curve: Curves.decelerate,
                     ),
               ),
             ),
             Positioned(
               bottom: 0,
-              child: Assets.images.splashBgBlurs.image(
-                width: mediaQuery.width,
-                fit: BoxFit.contain,
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.transparent, Colors.black],
+                    stops: [0.3, 0.6],
+                  ),
+                ),
+                child: Assets.images.splashBgBlurs.image(
+                  width: mediaQuery.width,
+                  fit: BoxFit.contain,
+                ),
               ),
             ),
             Positioned(
@@ -96,7 +115,7 @@ class SplashScreen extends StatelessWidget {
                   )
                   .animate(
                     onPlay: (controller) => controller.repeat(),
-                    delay: 1000.ms,
+                    delay: 2000.ms,
                   )
                   .shimmer(
                     color: Colors.redAccent,
@@ -106,10 +125,10 @@ class SplashScreen extends StatelessWidget {
                   )
                   .animate()
                   .slideY(
-                    duration: 1000.ms,
+                    duration: 2000.ms,
                     begin: 0,
                     end: -0.1,
-                    curve: Curves.easeInOut,
+                    curve: Curves.decelerate,
                   ),
             ),
             ...cloudCurtain(mediaQuery),
@@ -122,9 +141,43 @@ class SplashScreen extends StatelessWidget {
   List<Positioned> cloudCurtain(Size mediaQuery) {
     return [
       Positioned(
-        right: -30,
+        top: mediaQuery.height * 0.03,
+        left: 0,
+        child: Assets.images.cloudInverted
+            .image(
+              height: 300,
+            )
+            .animate(
+                //   onPlay: (controller) => controller.repeat(),
+                )
+            .slideX(
+              duration: 2200.ms,
+              begin: 0,
+              end: -0.82,
+              curve: Curves.decelerate,
+            ),
+      ),
+      Positioned(
+        top: mediaQuery.height * 0.10,
+        right: 0,
+        child: Assets.images.cloudOriginal
+            .image(
+              height: 300,
+            )
+            .animate(
+                //   onPlay: (controller) => controller.repeat(),
+                )
+            .slideX(
+              duration: 2000.ms,
+              begin: 0,
+              end: 0.81,
+              curve: Curves.decelerate,
+            ),
+      ),
+      Positioned(
         top: mediaQuery.height * 0.2,
-        child: Assets.images.cloudOriginal
+        left: 0,
+        child: Assets.images.cloudInverted
             .image(
               height: 300,
             )
@@ -132,32 +185,15 @@ class SplashScreen extends StatelessWidget {
                 //   onPlay: (controller) => controller.repeat(),
                 )
             .slideX(
-              duration: 1800.ms,
+              duration: 2000.ms,
               begin: 0,
-              end: 0.7,
-              curve: Curves.easeInOut,
+              end: -0.82,
+              curve: Curves.decelerate,
             ),
       ),
       Positioned(
-        right: 60,
-        top: mediaQuery.height * 0.2,
-        child: Assets.images.cloudOriginal
-            .image(
-              height: 300,
-            )
-            .animate(
-                //   onPlay: (controller) => controller.repeat(),
-                )
-            .slideX(
-              duration: 1000.ms,
-              begin: 0,
-              end: -0.7,
-              curve: Curves.easeInOut,
-            ),
-      ),
-      Positioned(
-        right: 10,
         top: mediaQuery.height * 0.3,
+        right: 0,
         child: Assets.images.cloudOriginal
             .image(
               height: 300,
@@ -166,15 +202,32 @@ class SplashScreen extends StatelessWidget {
                 //   onPlay: (controller) => controller.repeat(),
                 )
             .slideX(
-              duration: 1800.ms,
+              duration: 2000.ms,
               begin: 0,
-              end: -0.7,
-              curve: Curves.easeInOut,
+              end: 0.82,
+              curve: Curves.decelerate,
             ),
       ),
       Positioned(
-        right: -40,
-        top: mediaQuery.height * 0.3,
+        top: mediaQuery.height * 0.35,
+        left: 0,
+        child: Assets.images.cloudInverted
+            .image(
+              height: 300,
+            )
+            .animate(
+                //   onPlay: (controller) => controller.repeat(),
+                )
+            .slideX(
+              duration: 2000.ms,
+              begin: 0,
+              end: -0.82,
+              curve: Curves.decelerate,
+            ),
+      ),
+      Positioned(
+        top: mediaQuery.height * 0.45,
+        left: 0,
         child: Assets.images.cloudOriginal
             .image(
               height: 300,
@@ -183,16 +236,16 @@ class SplashScreen extends StatelessWidget {
                 //   onPlay: (controller) => controller.repeat(),
                 )
             .slideX(
-              duration: 1800.ms,
+              duration: 2000.ms,
               begin: 0,
-              end: 0.7,
-              curve: Curves.easeInOut,
+              end: -0.81,
+              curve: Curves.decelerate,
             ),
       ),
       Positioned(
-        right: -30,
         top: mediaQuery.height * 0.4,
-        child: Assets.images.cloudOriginal
+        right: 0,
+        child: Assets.images.cloudInverted
             .image(
               height: 300,
             )
@@ -200,15 +253,15 @@ class SplashScreen extends StatelessWidget {
                 //   onPlay: (controller) => controller.repeat(),
                 )
             .slideX(
-              duration: 900.ms,
+              duration: 2000.ms,
               begin: 0,
-              end: 0.7,
-              curve: Curves.easeInOut,
+              end: 0.82,
+              curve: Curves.decelerate,
             ),
       ),
       Positioned(
-        right: -20,
-        top: mediaQuery.height * 0.5,
+        top: mediaQuery.height * 0.55,
+        left: 0,
         child: Assets.images.cloudOriginal
             .image(
               height: 300,
@@ -217,95 +270,61 @@ class SplashScreen extends StatelessWidget {
                 //   onPlay: (controller) => controller.repeat(),
                 )
             .slideX(
-              duration: 1000.ms,
+              duration: 2000.ms,
+              begin: 0,
+              end: -0.81,
+              curve: Curves.decelerate,
+            ),
+      ),
+      Positioned(
+        top: mediaQuery.height * 0.6,
+        right: 0,
+        child: Assets.images.cloudInverted
+            .image(
+              height: 300,
+            )
+            .animate(
+                //   onPlay: (controller) => controller.repeat(),
+                )
+            .slideX(
+              duration: 2000.ms,
+              begin: 0,
+              end: 0.81,
+              curve: Curves.decelerate,
+            ),
+      ),
+      Positioned(
+        top: mediaQuery.height * 0.65,
+        left: 0,
+        child: Assets.images.cloudOriginal
+            .image(
+              height: 300,
+            )
+            .animate(
+                //   onPlay: (controller) => controller.repeat(),
+                )
+            .slideX(
+              duration: 2000.ms,
+              begin: 0,
+              end: -0.81,
+              curve: Curves.decelerate,
+            ),
+      ),
+      Positioned(
+        top: mediaQuery.height * 0.75,
+        left: 0,
+        child: Assets.images.cloudInverted
+            .image(
+              height: 300,
+            )
+            .animate(
+                //   onPlay: (controller) => controller.repeat(),
+                )
+            .slideX(
+              duration: 2000.ms,
               begin: 0,
               end: -0.7,
-              curve: Curves.easeInOut,
-            ),
-      ),
-      Positioned(
-        right: 20,
-        top: mediaQuery.height * 0.52,
-        child: Assets.images.cloudOriginal
-            .image(
-              height: 300,
-            )
-            .animate(
-                //   onPlay: (controller) => controller.repeat(),
-                )
-            .slideX(
-              duration: 900.ms,
-              begin: 0,
-              end: 0.8,
-              curve: Curves.easeInOut,
-            ),
-      ),
-      Positioned(
-        right: -40,
-        top: mediaQuery.height * 0.6,
-        child: Assets.images.cloudOriginal
-            .image(
-              height: 300,
-            )
-            .animate(
-                //   onPlay: (controller) => controller.repeat(),
-                )
-            .slideX(
-              duration: 900.ms,
-              begin: 0,
-              end: -0.8,
-              curve: Curves.easeInOut,
-            ),
-      ),
-      Positioned(
-        right: -20,
-        top: mediaQuery.height * 0.7,
-        child: Assets.images.cloudOriginal
-            .image(
-              height: 300,
-            )
-            .animate(
-                //   onPlay: (controller) => controller.repeat(),
-                )
-            .slideX(
-              duration: 900.ms,
-              begin: 0,
-              end: 0.8,
-              curve: Curves.easeInOut,
-            ),
-      ),
-      Positioned(
-        right: -30,
-        top: mediaQuery.height * 0.68,
-        child: Assets.images.cloudOriginal
-            .image(
-              height: 300,
-            )
-            .animate(
-                //   onPlay: (controller) => controller.repeat(),
-                )
-            .slideX(
-              duration: 900.ms,
-              begin: 0,
-              end: -0.8,
-              curve: Curves.easeInOut,
-            ),
-      ),
-      Positioned(
-        right: -20,
-        top: mediaQuery.height * 0.85,
-        child: Assets.images.cloudOriginal
-            .image(
-              height: 200,
-            )
-            .animate(
-                //   onPlay: (controller) => controller.repeat(),
-                )
-            .slideX(
-              duration: 1000.ms,
-              begin: 0,
-              end: -0.6,
-              curve: Curves.easeInOut,
+              curve: Curves.decelerate,
             ),
       ),
       Positioned(
@@ -319,10 +338,10 @@ class SplashScreen extends StatelessWidget {
                 //   onPlay: (controller) => controller.repeat(),
                 )
             .slide(
-              duration: 1000.ms,
+              duration: 2000.ms,
               begin: Offset.zero,
               end: const Offset(0.5, 0.38),
-              curve: Curves.easeInOut,
+              curve: Curves.decelerate,
             ),
       ),
     ];
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,15 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
 import 'package:midnight_suspense/app/app.dart';
 import 'package:midnight_suspense/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  bootstrap(
+    () => DevicePreview(
+      backgroundColor: Colors.black,
+      builder: (context) {
+        return const App();
+      },
+    ),
+  );
 }
