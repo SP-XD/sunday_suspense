215aeb8302480bad56c417aafa8579ca29cbfca1
sp-xd <69642436+SP-XD@users.noreply.github.com>
2024-07-25 17:48:02 +0530

    add new dependencies

--- android/app/src/main/kotlin/in/spxd/midnight/suspense/MainActivity.kt
+++ android/app/src/main/kotlin/in/spxd/midnight/suspense/MainActivity.kt
@@ -1,6 +1,7 @@
 package `in`.spxd.midnight.suspense
 
 import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity;
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity(), AudioServiceActivity() {
 }
