68ff7d18f332848c796d210402bc7933689ed8a5
sp-xd <69642436+SP-XD@users.noreply.github.com>
2024-07-28 00:25:57 +0530

    fix: update missing import paths from previous commit

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:midnight_suspense/l10n/l10n.dart';
+import 'package:midnight_suspense/src/l10n/l10n.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) {
