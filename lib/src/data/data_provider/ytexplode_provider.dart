eebbc22cc07afe671c378d974efbbe53e42716ad
sp-xd <69642436+SP-XD@users.noreply.github.com>
2024-09-03 18:13:18 +0530

    Add platform specific caching mechanisms

--- lib/src/data/data_provider/ytexplode_provider.dart
+++ lib/src/data/data_provider/ytexplode_provider.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
+import 'package:cronet_http/cronet_http.dart';
+import 'package:cupertino_http/cupertino_http.dart';
+import 'package:http/http.dart';
+import 'package:http/io_client.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class YtExplodeProvider {
@@ -6,7 +12,24 @@ class YtExplodeProvider {
   }
 
   YtExplodeProvider._() {
-    ytExplodeInstance = YoutubeExplode();
+    final Client httpClient;
+    if (Platform.isAndroid) {
+      final engine = CronetEngine.build(
+        cacheMode: CacheMode.memory,
+        cacheMaxSize: 2 * 1024 * 1024,
+      );
+      httpClient = CronetClient.fromCronetEngine(engine, closeEngine: true);
+    } else if (Platform.isIOS || Platform.isMacOS) {
+      final config = URLSessionConfiguration.ephemeralSessionConfiguration()
+        ..cache = URLCache.withCapacity(memoryCapacity: 2 * 1024 * 1024)
+        ..httpAdditionalHeaders = {'User-Agent': 'Book Agent'};
+      httpClient = CupertinoClient.fromSessionConfiguration(config);
+    } else {
+      httpClient = IOClient(HttpClient());
+    }
+
+    var ytHttpClient = YoutubeHttpClient(httpClient);
+    ytExplodeInstance = YoutubeExplode(ytHttpClient);
   }
 
   YoutubeExplode? ytExplodeInstance;
